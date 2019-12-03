6f63b7d10fba659e2fc968ab5f6d397e49031856
Zhenya Taran <zhenya@betmo.com>
2019-12-03 14:23:12 +0200

    components + state + hooks

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import Header from './common/Header';
+import CoursesPage from "./CoursesPage";
+
+function App() {
+    function getPage() {
+        const route = window.location.pathname;
+        if (route === "/about") return <AboutPage/>;
+        if (route === "/courses") return <CoursesPage/>;
+        return <HomePage/>;
+    }
+
+    return (
+        <div className="container-fluid">
+            <Header/>
+            {getPage()}
+        </div>
+    )
+}
+
+export default App;
