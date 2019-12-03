1b7ac83251bb956bbfc75cb1b77b6d82e6ab1a41
Zhenya Taran <zhenya@betmo.com>
2019-12-03 22:44:48 +0200

    router

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 function Header() {
+    const activeStyle = {color: "green"};
     return (
         <nav>
-            <a href="/">Home</a> | <a href="/courses">Course</a> | <a href="/about">About</a>
+            <NavLink activeStyle={activeStyle} exact to="/">Home</NavLink>
+            | <NavLink activeStyle={activeStyle} to="/courses">Course</NavLink>
+            | <NavLink activeStyle={activeStyle} to="/about">About</NavLink>
         </nav>
     )
 }
