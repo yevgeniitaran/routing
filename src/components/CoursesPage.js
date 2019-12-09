bb36e3123a9b5183e63139623cc2ebff2605fda0
Zhenya Taran <zhenya@betmo.com>
2019-12-09 23:03:03 +0200

    flux

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,13 +1,21 @@
 import React, {useState, useEffect} from "react";
-import {getCourses} from "../api/courseApi";
+import courseStore from '../stores/courseStore'
 import CourseList from "./CourseList";
 import {Link} from "react-router-dom";
+import {loadCourses, deleteCourse} from "../actions/courseAction";
 
 function CoursesPage() {
-    const [courses, setCourses] = useState([]);
+    const [courses, setCourses] = useState(courseStore.getCourses());
+
     useEffect(() => {
-        getCourses().then(_courses => setCourses(_courses));
+        courseStore.addChangeListener(onChange);
+        if (courseStore.getCourses().length === 0) loadCourses();
+        return () => courseStore.removeChangeListener(onChange);
     }, []);
+
+    function onChange() {
+       setCourses(courseStore.getCourses());
+    }
 
     return (
         <>
@@ -15,7 +23,7 @@ function CoursesPage() {
             <Link className="btn btn-primary" to="/course">
                 Add Course
             </Link>
-            <CourseList courses={courses}/>
+            <CourseList courses={courses} deleteCourse={deleteCourse}/>
         </>
     );
 }
